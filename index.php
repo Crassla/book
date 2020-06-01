d4d9c03a31eb85bf877b68e418b741865e3683f8
Crassla <64520589+Crassla@users.noreply.github.com>
2020-06-01 21:55:14 +1200

    Create index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php echo
+	
+<html>
+	<head>
+		<title>TBA</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<meta language="english">
+		<meta charset="UTF-8">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="author" content="Alexandria Meyer">
+		
+	
+	</head>
+	<body>
+		<div id="title">
+			<a href="index.html" class="titletext">Title</a> 
+		</div>
+		<div id="navigation">		
+			<span><a href="index.html" class="linkPages"><b>Home</b></a></span>
+			<span><a href="page2.html" class="linkPages">Page1</a></span>
+			<div class="dropdown">
+				<div class="dropdown-content">
+					<div id="dropdown">	
+						<div id="dropdowntext">
+							if needed
+							if needed
+						</div><!--dropdowntext2-->
+					</div><!--dropdown2-->
+				</div>
+			</div>
+			<span><a href="page3.html" class="linkPages">Page2</a></span>
+			<div class="dropdown">
+				<div class="dropdown-content">
+					<div id="dropdown">	
+						<div id="dropdowntext">	
+							if needed
+							if needed
+						</div><!--dropdowntext-->
+					</div><!--dropdown-->
+				</div><!--dropdowncontent-->
+			</div>
+			<div class="dropdown">
+			<span><a href="page4.html" class="linkPages">Contact Us</a></span>
+				<div class="dropdown-content">
+					<div id="dropdown">	
+						<div id="dropdowntext">
+							<h5><u>if needed</u></h5>
+							<h6><u>if needed</u></h6>
+						</div><!--dropdowntext4-->
+					</div><!--dropdown4-->
+				</div>
+			</div>
+	
+		</div> <!--navigation-->
+?>
